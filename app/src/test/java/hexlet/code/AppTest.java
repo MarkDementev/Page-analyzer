b4102b4c7c0908fd76a956568481565fa457aead
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2023-05-24 18:12:02 +0300

    add more tests

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,11 +1,13 @@
 package hexlet.code;
 
 import hexlet.code.domain.Url;
+import hexlet.code.domain.query.QUrl;
 
 import io.javalin.Javalin;
 
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,16 +16,17 @@ import org.junit.jupiter.api.Test;
 import io.ebean.Database;
 import io.ebean.DB;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
     private static final String PROJECT_TITLE = "Анализатор страниц";
-    private static final String URL_EXAMPLE = "Пример: https://www.example.com";
+    private static final String URL_EXAMPLE = "https://www.example.com";
     private static final String URLS_TABLE_FIRST_COLUMN_TITLE = "ID";
     private static final String URLS_TABLE_SECOND_COLUMN_TITLE = "Имя";
     private static Javalin app;
     private static String baseUrl;
-    private static Url existingUrl;
     private static Database database;
 
     @BeforeAll
@@ -50,26 +53,50 @@ public class AppTest {
     @Test
     void testIndex() {
         HttpResponse<String> response = Unirest.get(baseUrl).asString();
+
         assertThat(response.getStatus()).isEqualTo(200);
         assertThat(response.getBody()).contains(PROJECT_TITLE);
-        assertThat(response.getBody()).contains(URL_EXAMPLE);
     }
 
     @Test
     void testUrls() {
         HttpResponse<String> response = Unirest.get(baseUrl + "/urls").asString();
+
         assertThat(response.getStatus()).isEqualTo(200);
         assertThat(response.getBody()).contains(URLS_TABLE_FIRST_COLUMN_TITLE);
         assertThat(response.getBody()).contains(URLS_TABLE_SECOND_COLUMN_TITLE);
     }
 
-//    @Test
-//    void testAddUrl() {
-//        //проверка, что сначала нет инфы про урл тут
-//        //идёт добавка урла
-//        //проверка, что добавлено //через показВсех
-//        //добавка того же самого урла
-//        //проверка, что он не добавился ещё раз
-//    }
+    @Test
+    void testUrlWithoutAdd() {
+        HttpResponse<String> response = Unirest.get(baseUrl + "/urls/1").asString();
+
+        assertThat(response.getStatus()).isEqualTo(404);
+        assertThat(response.getBody()).doesNotContain(URL_EXAMPLE);
+    }
+
+    @Test
+    void testAddUrl() {
+        HttpResponse addUrlResponse = Unirest.post(baseUrl + "/urls")
+                .field("url", URL_EXAMPLE).asEmpty();
+        HttpResponse<String> checkAddedUrlResponse = Unirest.get(baseUrl + "/urls/1").asString();
+        Url addedUrlFromDB = new QUrl()
+                .name.equalTo(URL_EXAMPLE)
+                .findOne();
+
+        assertThat(addUrlResponse.getStatus()).isEqualTo(200);
+        assertThat(checkAddedUrlResponse.getStatus()).isEqualTo(200);
+        assertThat(addedUrlFromDB).isNotNull();
+        assertThat(checkAddedUrlResponse.getBody()).contains(URL_EXAMPLE);
+
+        HttpResponse repeatAddUrlResponse = Unirest.post(baseUrl + "/urls")
+                .field("url", URL_EXAMPLE).asEmpty();
+        List<Url> addedUrlsFromDB = new QUrl()
+                .name.equalTo(URL_EXAMPLE)
+                .findList();
+
+        assertThat(repeatAddUrlResponse.getStatus()).isEqualTo(200);
+        assertThat(addedUrlsFromDB.size() == 1).isTrue();
+    }
 }
 
